e61946cbdbeb98a0fb84bfb18d73f6cbd652a2db
lluisgomez <lgomez@cvc.uab.es>
2025-02-18 01:29:56 +0100

    generate images in single node using 4 gpus in parallel

--- src/generate_images_multigpu.py
+++ src/generate_images_multigpu.py
@@ -1,4 +1,4 @@
-import os
+import os,glob
 import logging
 import json
 import argparse
@@ -33,7 +33,7 @@ def generate_images(gpu_id, input_files, output_dir, batch_size=32):
             data = json.load(f)
         
         img_keys = list(data.keys())
-        img_captions_pairs = list(data.values())
+        img_captions = list(data.values())
 
         file_name = os.path.splitext(os.path.basename(input_file))[0]
         file_output_dir = os.path.join(output_dir, file_name)
@@ -41,22 +41,12 @@ def generate_images(gpu_id, input_files, output_dir, batch_size=32):
 
         for i in range(0, len(img_keys), batch_size):
             batch_keys = img_keys[i:i + batch_size]
-            batch_caption_pairs = img_captions_pairs[i:i + batch_size]
+            batch_captions = img_captions[i:i + batch_size]
             seeds = [random.randint(0, int(1e9)) for _ in range(len(batch_keys))]
 
-            first_batch_captions = [pair[0] for pair in batch_caption_pairs]
             start_time = time.time()
-            images_1 = pipe(
-                prompt=first_batch_captions,
-                num_inference_steps=2,
-                guidance_scale=0.0,
-                generator=[torch.manual_seed(seed) for seed in seeds],
-                width=256, height=256
-            ).images
-
-            second_batch_captions = [pair[1] for pair in batch_caption_pairs]
-            images_2 = pipe(
-                prompt=second_batch_captions,
+            images = pipe(
+                prompt=batch_captions,
                 num_inference_steps=2,
                 guidance_scale=0.0,
                 generator=[torch.manual_seed(seed) for seed in seeds],
@@ -65,11 +55,10 @@ def generate_images(gpu_id, input_files, output_dir, batch_size=32):
 
             batch_time = time.time() - start_time
 
-            for img_key, img1, img2 in zip(batch_keys, images_1, images_2):
-                img1.save(os.path.join(file_output_dir, f"{img_key}_1.jpg"), format="JPEG", quality=90)
-                img2.save(os.path.join(file_output_dir, f"{img_key}_2.jpg"), format="JPEG", quality=90)
+            for img_key, img in zip(batch_keys, images):
+                img.save(os.path.join(file_output_dir, f"{img_key}.jpg"), format="JPEG", quality=90)
 
-            total_images += len(images_1) + len(images_2)
+            total_images += len(images)
             total_time += batch_time
             throughput = total_images / total_time
             print(f"[GPU {gpu_id}] Processed {total_images} images at {throughput:.2f} images/sec")
@@ -83,15 +72,17 @@ if __name__ == "__main__":
     diffusers.utils.logging.set_verbosity(diffusers.logging.CRITICAL)
 
     parser = argparse.ArgumentParser(description="Generate images with multi-GPU data parallelism")
-    parser.add_argument('--input_files', type=str, nargs='+', required=True, help='Paths to the JSON input files')
+    parser.add_argument('--input_files', type=str, required=True, help='Paths to the JSON input files')
     parser.add_argument('--output_dir', type=str, required=True, help='Directory to save generated images')
     parser.add_argument('--batch_size', type=int, default=32, help='Batch size for inference')
     parser.add_argument('--num_gpus', type=int, default=4, help='Number of GPUs to use')
     args = parser.parse_args()
+
+    input_files = glob.glob(args.input_files+'/*json')
     
     # Split the input files evenly among the available GPUs
     gpu_inputs = [[] for _ in range(args.num_gpus)]
-    for idx, input_file in enumerate(args.input_files):
+    for idx, input_file in enumerate(input_files):
         gpu_inputs[idx % args.num_gpus].append(input_file)
     
     processes = []
