6d8fcdc5ea41085d6b8223c08d75ea17976780d4
lluisgomez <lgomez@cvc.uab.es>
2025-02-19 01:28:03 +0100

    add script to recover the shards from the temp folders

---
+++ data_pipeline/05_reshard_small.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Define the source directory containing the modified shards and the destination for the new tar files
+SRC_DIR="/gpfs/scratch/ehpc42/datasets/datacomp/small_hybrid/tmp"
+DEST_DIR="/gpfs/scratch/ehpc42/datasets/datacomp/small_hybrid/shards"
+mkdir -p "$DEST_DIR"
+
+# Use find to safely iterate over directories without globbing issues
+find "$SRC_DIR" -mindepth 1 -maxdepth 1 -type d -print0 | while IFS= read -r -d '' shard; do
+    base=$(basename "$shard")
+    echo "Creating tar archive for: $base"
+
+    # Create a tar file that contains only the contents of the shard directory.
+    tar cf "$DEST_DIR/$base.tar" -C "$shard" .
+done
